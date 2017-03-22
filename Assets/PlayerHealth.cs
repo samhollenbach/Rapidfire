fc5f841ddf34ae2c368104331baf6305bb9ef0de
lbusch25 <lbusch@macalester.edu>
2017-03-22 15:10:04 -0500

    Got sliders working in PlayerHealth

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -26,24 +26,25 @@ public class PlayerHealth : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		currentHP = HP;
-//		healthSlider = GameObject.Find ("HUDCanvas/healthGUI/HealthSlider");
-//		hurtImage = GameObject.Find ("HUDCanvas/healthGUI/DamageImage");
+		healthSlider = GameObject.Find ("HUDCanvas/healthGUI/Slider").GetComponent<Slider>();
+		hurtImage = GameObject.Find ("HUDCanvas/Image").GetComponent<Image>();
+		healthSlider.value = currentHP;
 	}
 
 	// Update is called once per frame
 	void Update () {
-//		if (hurt) {
-//			hurtImage.color = flashColor;
-//		} else {
-//			hurtImage.color = Color.Lerp (hurtImage.color, Color.clear, flashSpeed * Time.deltaTime);
-//		} 
+		if (hurt) {
+			hurtImage.color = flashColor;
+		} else {
+			hurtImage.color = Color.Lerp (hurtImage.color, Color.clear, flashSpeed * Time.deltaTime);
+		} 
 		hurt = false;
 	}
 
 	public void Hurt(int damage) {
 		hurt = true;
 		currentHP -= damage;
-		//healthSlider.value = currentHP;
+		healthSlider.value = currentHP;
 		if (!dead) {
 			Death ();
 		}
@@ -52,7 +53,7 @@ public class PlayerHealth : MonoBehaviour {
 	public void Death() {
 		if (HP <= 0) {
 			dead = true;
-			Destroy (gameObject);
+			NetworkServer.Destroy (gameObject);
 		}
 	}
 }
