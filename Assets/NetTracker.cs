fe7f16ef0f8aa8b05c45adbb8c04bd2d50921039
Sam Hollenbach <shollenb@macalester.edu>
2017-03-31 14:22:32 -0500

    Added Net Tracker

---
+++ Assets/NetTracker.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetTracker : NetworkBehaviour {
+
+	[SyncVar(hook = "FacingCallback")]
+	public bool netFacingRight = true;
+
+	[Command]
+	public void CmdFlipSprite(bool facing)
+	{
+		netFacingRight = facing;
+		if (netFacingRight)
+		{
+			Vector3 SpriteScale = transform.localScale;
+			float scaleX = SpriteScale.x;
+			SpriteScale.x = Mathf.Abs(scaleX);
+			transform.localScale = SpriteScale;
+		}
+		else
+		{
+			Vector3 SpriteScale = transform.localScale;
+			float scaleX = SpriteScale.x;
+			SpriteScale.x = -Mathf.Abs(scaleX);
+			transform.localScale = SpriteScale;
+		}
+	}
+
+	void FacingCallback(bool facing)
+	{
+		netFacingRight = facing;
+		if (netFacingRight)
+		{
+			Vector3 SpriteScale = transform.localScale;
+			float scaleX = SpriteScale.x;
+			SpriteScale.x = Mathf.Abs(scaleX);
+			transform.localScale = SpriteScale;
+		}
+		else
+		{
+			Vector3 SpriteScale = transform.localScale;
+			float scaleX = SpriteScale.x;
+			SpriteScale.x = -Mathf.Abs(scaleX);
+			transform.localScale = SpriteScale;
+		}
+	}
+}
