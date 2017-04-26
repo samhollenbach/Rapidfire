e2013bf71b556a698311c9116f22c17a9b0e2024
Sam Hollenbach <shollenb@macalester.edu>
2017-04-26 14:43:18 -0500

    Lobby test 2

--- Assets/scripts/StartButton.cs
+++ Assets/scripts/StartButton.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
 
 public class StartButton : MonoBehaviour {
 	
 	public void StartGameButton(int sceneIndex){
 		SceneManager.LoadScene (sceneIndex);
+		NetworkLobbyManager.singleton.GetComponent<NetworkManagerHUD> ().enabled = true;
 	}
 
 }
