31d0807df1a0662325e01d5e1048e177a57965cd
Sam Hollenbach <shollenb@macalester.edu>
2017-04-26 14:50:56 -0500

    Lobby test 4

--- Assets/scripts/StartButton.cs
+++ Assets/scripts/StartButton.cs
@@ -8,9 +8,9 @@ public class StartButton : MonoBehaviour {
 	
 	public void StartGameButton(int sceneIndex){
 		SceneManager.LoadScene (sceneIndex);
-		if (sceneIndex == 1) {
-			NetworkLobbyManager.singleton.GetComponent<NetworkManagerHUD> ().enabled = true;
-		}
+//		if (sceneIndex == 1) {
+//			NetworkLobbyManager.singleton.GetComponent<NetworkManagerHUD> ().enabled = true;
+//		}
 	}
 
 }
