bbbe04a509f6156a1dc48b97531ff9ba3b4e1d49
Sam Hollenbach <shollenb@macalester.edu>
2017-04-28 15:02:47 -0500

    kill an error final

--- Assets/scripts/HealthBar.cs
+++ Assets/scripts/HealthBar.cs
@@ -49,8 +49,7 @@ public class HealthBar : NetworkBehaviour{
 		try{
 			HealthBarPosition = transform.position + new Vector3 (0, collider.size.y * 3.5f, 0);
 		}catch(Exception e){
-			print ("Test");
-			Debug.Log (e);
+			
 		}
 
 
