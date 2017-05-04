11ba68d063a33afba8236908b0ae1349719b7675
Sam Hollenbach <shollenb@macalester.edu>
2017-05-03 20:13:06 -0500

    big walls

--- Assets/scripts/CameraFollow.cs
+++ Assets/scripts/CameraFollow.cs
@@ -34,77 +34,11 @@ public class CameraFollow : MonoBehaviour
 			float x = transform.position.x;
 			float y = transform.position.y;
 
-			Transform wallLeft = GameObject.Find("WallLeft").transform;
-			Transform wallRight = GameObject.Find("WallRight").transform;
-			Transform floor = GameObject.Find("Floor").transform;
-			Transform ceiling = GameObject.Find("Ceiling").transform;
-
-			Camera playerCam = player.GetComponent<PlayerControl> ().playerCam;
-
-			 
-			Vector3 playerPos = new Vector3 (playerTransform.position.x, playerTransform.position.y,0);
-			Vector3 lWPos = new Vector3 (wallLeft.position.x, wallLeft.position.y,0);
-			Vector3 rWPos = new Vector3 (wallRight.position.x, wallRight.position.y,0);
-			Vector3 fPos = new Vector3 (floor.position.x, floor.position.y,0);
-			Vector3 cPos = new Vector3 (ceiling.position.x, ceiling.position.y,0);
-//			Vector3 playerScreen = playerCam.WorldToScreenPoint (playerPos);
-//			Vector3 leftWallPos = playerCam.WorldToScreenPoint (lWPos);
-//			Vector3 rightWallPos = playerCam.WorldToScreenPoint (rWPos);
-//			Vector3 floorPos = playerCam.WorldToScreenPoint (fPos);
-//			Vector3 ceilPos = playerCam.WorldToScreenPoint (cPos);
-
-			Vector3 playerScreen = playerCam.WorldToViewportPoint (playerPos);
-			Vector3 leftWallPos = playerCam.WorldToViewportPoint (lWPos);
-			Vector3 rightWallPos = playerCam.WorldToViewportPoint (rWPos);
-			Vector3 floorPos = playerCam.WorldToViewportPoint (fPos);
-			Vector3 ceilPos = playerCam.WorldToViewportPoint (cPos);
-
-			float xMax = 0;
-			float xMin = 0;
-			float yMax = 0;
-			float yMin = 0;
-
-
-//			print (w);
-//			print (h);
-//			print (lWPos);
-//			print (rWPos);
-//			print (fPos);
-//			print (cPos);
-//			print (leftWallPos);
-//			print (rightWallPos);
-//			print (floorPos);
-//			print (ceilPos);
-//
-//			float viewportX = .5f;
-//			float viewportY = .5f;
-//
-//			if (leftWallPos.x < 0) {
-//				viewportX = 0f;
-//			} else if (rightWallPos.x > 1) {
-//				viewportX = 1f;
-//			} else {
-//				viewportX = .5f;
-//			}
-//			if (ceilPos.y < 0) {
-//				viewportY = 0f;
-//			} else if (floorPos.x > 1) {
-//				viewportY = 1f;
-//			} else {
-//				viewportY = .5f;
-//			}
-//			Vector3 camPos = new Vector3 (x,y,-10);
-//			Vector3 worldCam = playerCam.ViewportToWorldPoint(camPos);
-//
-//			transform.position = new Vector3(worldCam.x,worldCam.y,depth);
-//			transform.position = worldCam;
-
-
 			//Keeps the camera within the bounds of the map (will use map size variables soon)
-			if (playerTransform.position.x >= 80) {
-				x = 82;
-			} else if (playerTransform.position.x <= -80) {
-				x = -82;
+			if (playerTransform.position.x >= 90) {
+				x = 90;
+			} else if (playerTransform.position.x <= -90) {
+				x = -90;
 			} else {
 				x = playerTransform.position.x;
 			}
